bd2a218ee3bc12d7a00206b7fbdb48a3e7945bf9
triks <trik0001000@gmail.com>
2024-03-26 10:10:13 +0300

    настроил `hello.html` со стольными проблемы

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5561'))
+        PORT = int(os.environ.get('SERVER_PORT', '5562'))
     except ValueError:
         PORT = 5555
 
